bf4bc2d9056a772a662a137d06345cdcd294bb02
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-07-14 18:01:25 +0600

    interactivity with Javascript course

---
+++ Practise/JS/js.js
@@ -0,0 +1,20 @@
+function openMe(){
+		 x=document.getElementById("o");
+		 x.className="open";
+		 y=document.getElementById("c");
+		 y.className="display";
+		}
+
+		function closeMe(){
+			 x=document.getElementById("c");
+		 x.className="close";
+		  y=document.getElementById("o");
+		 y.className="display";
+		}
+
+		function None(){
+			 x=document.getElementById("o");
+		 x.className="display";
+		 y=document.getElementById("c");
+		 y.className="display";
+		}
